cd9e12040be83448bae7efbfa0292615b21ed694
HoverGo <kotikarbuz@gmail.com>
2024-06-26 21:43:07 +0500

    settins

--- GSbackend/settings.py
+++ GSbackend/settings.py
@@ -76,9 +76,14 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://greenshop-backend-production.up.railway.app",
+]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://greenshop-backend-production.up.railway.app",
+]
 
 ROOT_URLCONF = "GSbackend.urls"
 
